deab321651fe9836d3dd96f0b4ee59fc79fd2be8
Abdulwahab E A E Haidar <wahhab.eisa+GITHUB@gmail.com>
2026-02-06 16:14:42 +0000

    Created the main Python file.

---
+++ main.py
@@ -0,0 +1,4 @@
+from config import start as main # Importation of the "engine purpose" function
+
+if __name__ == "__main__":  # Starts the "app", if the main file "main.py" is run
+    main() # Function called. Application Starts.
